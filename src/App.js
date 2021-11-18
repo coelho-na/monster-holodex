6f0efe3c94b545c1e15da5a2a6faab2a61e00a55
Nadine Coelho <nadinecoelho9@gmail.com>
2021-11-18 19:59:17 -0300

    add search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/search-box.jsx";
 import "./App.css";
 
 class App extends Component {
@@ -7,23 +8,36 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
-  async getMonstersName() {
+  getMonstersName = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
     this.setState({ monsters: data });
-  }
+  };
 
   componentDidMount() {
     this.getMonstersName();
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}></CardList>
+        <SearchBox
+          placeholder="Search Monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
